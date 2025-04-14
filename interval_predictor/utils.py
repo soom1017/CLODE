db8e7311933b8a1b792ba0cb0aa458ca9acd3a91
ByungWanLim <pook0612@naver.com>
2025-04-14 12:59:29 +0900

    r2 score 추가

--- interval_predictor/utils.py
+++ interval_predictor/utils.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 def plot_regression_results(y_true, y_pred, title, save_path=None):
     mae = np.mean(np.abs(y_pred - y_true))
     rmse = np.sqrt(np.mean((y_pred - y_true) ** 2))
+    r2 = r2_score(y_true, y_pred)
     
     print(f'{title} \n\t- MAE: {mae:.4f}, RMSE: {rmse:.4f}')
 
